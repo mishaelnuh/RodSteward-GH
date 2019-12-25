9955c3f88df93bb022bb260ef270cca1935402bf
Mishael Nuh <mishael.ebel.nuh@gmail.com>
2019-12-24 22:56:55 -0500

    Remove unused dependencies

--- RodSteward/OutputRodCutSVG.cs
+++ RodSteward/OutputRodCutSVG.cs
@@ -1,17 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Grasshopper.Kernel;
-using Grasshopper.GUI;
-using Grasshopper.GUI.Canvas;
-using Rhino.Geometry;
 using System.IO;
 using Svg;
 using Grasshopper;
 using Grasshopper.Kernel.Data;
-using Grasshopper.Kernel.Types;
 
 namespace RodSteward
 {
